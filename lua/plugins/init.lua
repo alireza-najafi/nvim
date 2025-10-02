c523d0cee5387742ef81e9b8109bc3454b745a2d
Alireza Yazdi <alireza@Alirezanioffice.anyonsys.com>
2025-10-01 21:51:51 -0400

    venv detect

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -113,6 +113,24 @@ return {
     -- See Commands section for default commands if you want to lazy load on them
   },
 
+  {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+  },
+  ft = "python", -- Load when opening Python files
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+      search = {}, -- if you add your own searches, they go here.
+      options = {} -- if you add plugin options, they go here.
+  },
+},
+
+
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
